ce8622561bf9e60be373be944773a9cffb77697e
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-16 16:41:11 +0200

    update test

--- snacks_crud_project/snacks/tests.py
+++ snacks_crud_project/snacks/tests.py
@@ -18,18 +18,18 @@ class SnackTests(TestCase):
     def test_string_representation(self):
         self.assertEqual(str(self.snack), "cake")
 
-    def test_thing_content(self):
+    def test_snack_content(self):
         self.assertEqual(f"{self.snack.title}", "cake")
         self.assertEqual(f"{self.snack.purchaser}", "tester")
         self.assertEqual(self.snack.description, 'sweet')
 
-    def test_thing_list_view(self):
+    def test_snack_list_view(self):
         response = self.client.get(reverse("snack_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "cake")
         self.assertTemplateUsed(response, "snack_list.html")
 
-    def test_thing_detail_view(self):
+    def test_snack_detail_view(self):
         response = self.client.get(reverse("snack_detail", args="1"))
         no_response = self.client.get("/100000/")
         self.assertEqual(response.status_code, 200)
@@ -37,7 +37,7 @@ class SnackTests(TestCase):
         self.assertContains(response, "purchaser: tester")
         self.assertTemplateUsed(response, "snack_detail.html")
 
-    def test_thing_create_view(self):
+    def test_snack_create_view(self):
         response = self.client.post(
             reverse("snack_create"),
             {
@@ -52,7 +52,7 @@ class SnackTests(TestCase):
 
 
 
-    def test_thing_update_view_redirect(self):
+    def test_snack_update_view_redirect(self):
         response = self.client.post(
             reverse("snack_update", args="1"),
             {"title": "Updated title","description": "saltys","purchaser":self.user.id}
@@ -60,7 +60,7 @@ class SnackTests(TestCase):
 
         self.assertRedirects(response, reverse("snack_detail", args="1"))
 
-    def test_thing_delete_view(self):
+    def test_snack_delete_view(self):
         response = self.client.get(reverse("snack_delete", args="1"))
         self.assertEqual(response.status_code, 200)
 
